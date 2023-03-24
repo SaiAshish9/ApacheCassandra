d530392b0e923790fcee591ab8c931973e1cfd4f
SaiAshish <saiashish3760@gmail.com>
2023-03-24 21:22:08 +0530

    Add files via upload

---
+++ src/main/java/com/sai/spring/cassandra/api/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.sai.spring.cassandra.api.repository;
+
+import java.util.List;
+
+import org.springframework.data.cassandra.repository.AllowFiltering;
+import org.springframework.data.cassandra.repository.CassandraRepository;
+
+import com.sai.spring.cassandra.api.model.User;
+
+public interface UserRepository extends CassandraRepository<User, Integer> {
+
+	@AllowFiltering
+	List<User> findByAgeGreaterThan(int age);
+
+}
